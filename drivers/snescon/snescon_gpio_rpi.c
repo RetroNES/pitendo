1e665867e4b02ea5521d9e11f9569d23f563ea2a
chrisis <chriistian87@gmail.com>
2015-03-08 21:53:01 +0100

    Updated list of valid GPIOs

--- drivers/snescon/snescon_gpio_rpi.c
+++ drivers/snescon/snescon_gpio_rpi.c
@@ -57,7 +57,7 @@ static volatile unsigned *gpio;	// I/O access.
 /*
  * All valid GPIOs found on the Raspberry Pi P1 Header.
  */
-static const unsigned char all_valid_gpio[] = { 0, 1, 2, 3, 4, 7, 8, 9, 10, 11, 14, 15, 17, 18, 21, 22, 25, 27 };
+static const unsigned char all_valid_gpio[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27 };
 
 /**
  * Set GPIO high.
